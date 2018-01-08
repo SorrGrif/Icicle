6e0efe282df7ee200a6c81734915bdcb709c0f3a
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-01-08 17:55:45 -0500

    created viewcontroller for the profile viewcontroller

---
+++ Icicle/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  Icicle
+//
+//  Created by Griffin Sorrentino on 2018-01-08.
+//  Copyright © 2018 Griffin Sorrentino. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
