01ec5e8ad3bc539930a99a237c35c9e0cc45670f
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-01-10 13:10:24 -0500

    created the viewcontroller class and viewcontroller for the messenger

---
+++ Icicle/MessengerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessengerViewController.swift
+//  Icicle
+//
+//  Created by Griffin Sorrentino on 2018-01-09.
+//  Copyright © 2018 Griffin Sorrentino. All rights reserved.
+//
+
+import UIKit
+
+
+class MessangerViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
