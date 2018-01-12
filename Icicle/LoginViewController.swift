9b9139b168400b3ea35c2bee4ca50477ff0129c8
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-01-12 14:25:34 -0500

    made a custom animation for when the password is typed incorrectly

--- Icicle/LoginViewController.swift
+++ Icicle/LoginViewController.swift
@@ -26,19 +26,33 @@ class LoginViewController: UIViewController {
 
     @IBAction func LoginButtonOnClick(_ sender: Any)
     {
+        
+        
         //when the login button is clicked, attempt to login with the given username and password
         let username = UserNameTextField.text!
         let password = PasswordTextField.text!
         
         let user = User(currentUser: username)
         
-        FooterLabel.text = "Incorrect Username!"
-        if(user.username == username)
+        //if the username is incorrect display that as a message and shake the username field
+        if(user.username != username)
         {
-            FooterLabel.text = "Incorrect Password!"
-            if(user.password == password)
+            FooterLabel.text = "Incorrect Username!"
+            FooterLabel.textColor = .red
+            incorrectInfo(fieldToShake: UserNameTextField)
+        }
+        else
+        {
+            //if the password is incorrect display that as a message and shake the password field
+            if(user.password != password)
             {
-                FooterLabel.text = "Remember: Don't share you login credentials"
+                FooterLabel.text = "Incorrect Password!"
+                FooterLabel.textColor = .red
+                incorrectInfo(fieldToShake: PasswordTextField)
+            }
+            else
+            {
+                FooterLabel.text = "Don't share your login credentials"
                 login(user: user)
             }
         }
@@ -54,6 +68,18 @@ class LoginViewController: UIViewController {
         defaults.set(user.username, forKey: "user")
         defaults.set(true, forKey: "loggedin")
         performSegue(withIdentifier: "LoginSegue", sender: self)
+    }
+    
+    func incorrectInfo(fieldToShake: UITextField)
+    {
+        //this will create a new animation object and whatever UITextField you send this method it will shake it
+        let shakeAnim = CABasicAnimation(keyPath: "position")
+        shakeAnim.duration = 0.1
+        shakeAnim.repeatCount = 2
+        shakeAnim.autoreverses = true
+        shakeAnim.fromValue = NSValue(cgPoint: CGPoint(x: fieldToShake.center.x - 10, y: fieldToShake.center.y))
+        shakeAnim.toValue = NSValue(cgPoint: CGPoint(x:fieldToShake.center.x + 10, y: fieldToShake.center.y))
+        fieldToShake.layer.add(shakeAnim, forKey: "position")
     }
 
 }
