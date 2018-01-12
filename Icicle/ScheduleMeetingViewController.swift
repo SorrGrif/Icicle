d3120ec6ec34da3965ee0b504b20ca31e136762f
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-01-12 12:58:36 -0500

    added comments

--- Icicle/ScheduleMeetingViewController.swift
+++ Icicle/ScheduleMeetingViewController.swift
@@ -48,60 +48,72 @@ class ScheduleMeetingViewController: UIViewController
         formatter.dateFormat = timeFormat
         let time = formatter.string(from: date)
         
-        //create a date and time string for fomatting purposes
-        let dateString = month
-        let timeString = time
-        
         //not sure what the .normal does but it makes it not throw an error so meh.
         //set the title of the buttons to the current date and time
-        FromDateButton.setTitle(dateString, for: .normal)
-        FromTimeButton.setTitle(timeString, for: .normal)
+        FromDateButton.setTitle(month, for: .normal)
+        FromTimeButton.setTitle(time, for: .normal)
         
-        UntilDateButton.setTitle(dateString, for: .normal)
-        UntilTimeButton.setTitle(timeString, for: .normal)
+        UntilDateButton.setTitle(month, for: .normal)
+        UntilTimeButton.setTitle(time, for: .normal)
     }
     
     
     @IBAction func PickDate(_ sender: UIButton)
     {
+        //if a date button is pressed make the datepicker visible
         DatePickerNavBar.isHidden = false
-        DatePickerNavBarTitle.title = "Pick Date"
         DatePicker.isHidden = false
+        
+        //set the title to pick date and the mode to a date
+        DatePickerNavBarTitle.title = "Pick Date"
         DatePicker.datePickerMode = .date
         
-        
+        //set the current button to the one that was tapped
         currentButton = sender
+        
+        //and the button type of an enum of type date
         currentButtonType = .date
     }
     
     @IBAction func PickTime(_ sender: UIButton)
     {
+        //if a time button is pressed make the datepicker visible
         DatePickerNavBar.isHidden = false
-        DatePickerNavBarTitle.title = "Pick Time"
         DatePicker.isHidden = false
-        DatePicker.datePickerMode = .time
         
+        //set the title to pick time and the mode to a time
+        DatePickerNavBarTitle.title = "Pick Time"
+        DatePicker.datePickerMode = .date
+        
+        //set the current button to the one that was tapped
         currentButton = sender
+        
+        //and the button type of an enum of type time
         currentButtonType = .time
     }
     
     @IBAction func Done(_ sender: Any)
     {
+        //if the done button was pressed hide the datepicker view
         DatePickerNavBar.isHidden = true
         DatePicker.isHidden = true
         
+        //if the current button type is date, format the string for a date
         if(currentButtonType == .date)
         {
             formatter.dateFormat = monthFormat
-            let month = formatter.string(from: DatePicker.date)
+            let date = formatter.string(from: DatePicker.date)
             
-            currentButton.setTitle(month, for: .normal)
+            //set the title of the button to the date that was selected
+            currentButton.setTitle(date, for: .normal)
         }
+        //if the current button type is time, format the string for a time
         else if (currentButtonType == .time)
         {
             formatter.dateFormat = timeFormat
             let time = formatter.string(from: DatePicker.date)
             
+            //set the title of the button to the time that was selected
             currentButton.setTitle(time, for: .normal)
         }
         
@@ -110,6 +122,7 @@ class ScheduleMeetingViewController: UIViewController
     
     @IBAction func Cancel(_ sender: Any)
     {
+        //if the cancel button was pressed simply make the datepicker invisible
         DatePickerNavBar.isHidden = true
         DatePicker.isHidden = true
     }
