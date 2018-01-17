f4d563e069278adc126aa7c02f0bd31429f3e068
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2018-01-17 10:28:47 -0500

    made a 3 stage view for reporting a bug, finished the layout for stage 1

---
+++ Icicle/ReportBugStepOneViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ReportBugStepOneViewController.swift
+//  Icicle
+//
+//  Created by Griffin Sorrentino on 2018-01-17.
+//  Copyright © 2018 Griffin Sorrentino. All rights reserved.
+//
+
+import UIKit
+
+class ReportBugStepOneViewController: UIViewController
+{
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
---
+++ Icicle/ReportBugStepTwoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ReportBugStepTwoViewController.swift
+//  Icicle
+//
+//  Created by Griffin Sorrentino on 2018-01-17.
+//  Copyright © 2018 Griffin Sorrentino. All rights reserved.
+//
+
+import Foundation
